39b8311c0b8f72bc7994a2a47ed96ba4f929ddb0
tsuchiya kaita <tsuchiyakaita@arsaga.jp>
2021-02-10 16:40:28 +0900

    viewを表示するための実装を完了

---
+++ backend/app/Http/Controllers/TestController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    // SNS投稿のAPIの動作をテストするためのviewを返すaction
+    public function index()
+    {
+        return view('testView');
+    }
+}
