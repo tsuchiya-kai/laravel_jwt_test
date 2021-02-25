5a8cea4221f03d59674571a13bba6392bbb77d9f
土屋魁太 <tsuchiyakaita@arsaga.jp>
2021-02-25 17:03:18 +0900

    cors対策を雑に実装

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 // tymon/jwt-auth の検証
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', 'AuthController@login');
@@ -27,4 +23,7 @@ Route::group(['prefix' => 'auth', 'middleware' => 'auth:api'], function () {
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
     Route::post('me', 'AuthController@me');
+    Route::get('user', function (Request $request) {
+        return $request->user();
+    });
 });
