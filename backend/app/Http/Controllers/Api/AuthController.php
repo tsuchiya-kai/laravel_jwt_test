1f5e1ce08a8df0041ff1c540c902cd4e9aed064a
土屋魁太 <tsuchiyakaita@arsaga.jp>
2021-03-09 16:02:34 +0900

    不要な記述を削除

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -24,7 +24,6 @@ class AuthController extends Controller
      */
     public function login()
     {
-        return ['test'];
         $credentials = request(['email', 'password']);
 
         if (!$token = Auth::attempt($credentials)) {
